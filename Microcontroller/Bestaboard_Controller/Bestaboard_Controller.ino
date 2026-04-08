da7b635f95e7579204f39e9d53a66537a94652d5
ikrasmus <ikrasmusson@gmail.com>
2026-04-07 20:50:18 -0400

    3D Model and Code Update
    Improved sensor wire routing, and changed firmware to run on Arduino Nano. Also added pushbutton inputs into interrupt routine.

--- Microcontroller/Bestaboard_Controller/Bestaboard_Controller.ino
+++ Microcontroller/Bestaboard_Controller/Bestaboard_Controller.ino
@@ -2,40 +2,41 @@
 //#include <MultiStepper.h>
 
 //Define output locations
-const int S_IN = 13; //Serial Data
-const int CLK = 11; //Clock for shift register shifting
-const int STROBE = 9; //Updates outputs
-const int HOME = 7; //Home command.
+const int DATA = 12; //Serial Data
+const int SHIFT = 11; //Clock for shift register shifting
+const int STROBE = 10; //Latch shift register data
+const int HOME = 9; //Home command.
 
 //Define input locations
-const int ALL_HOMED = 5; // All charachters are homed.
-const int DIAG_3 = 4;
-const int DIAG_2 = 3;
+const int ALL_HOMED = 8; // All charachters are homed.
+const int CHANGE_DELAY = 3;
 const int DIAG_1 = 2;
 
 //Constants
-//Define time delay between pulses and servo sequence.
-const int c_DELAY = 50000;
 //byte SERVO_SEQUENCE [8] = {0b0001,0b0011,0b0010,0b0110,0b0100,0b1100,0b1000,0b1001};
 const byte c_SERVO_SEQUENCE [8] = {0b1110,0b1100,0b1101,0b1001,0b1011,0b0011,0b0111,0b0110};
 //byte SERVO_SEQUENCE3 [4] = {0b0011,0b0101,0b1100,0b1010};
 //byte SERVO_SEQUENCE4 [4] = {0b1110,0b1101,0b1011,0b0111};
 const int c_SERVO_COUNT = 2;
 const int c_SERVO_POS_COUNT = 4;
+const int c_DELAY_SETTING [3] = {50000, 5000, 1000};
 
+//Interrupt variables
+volatile int v_DELAY = c_DELAY_SETTING[0]; //Define time delay between pulses and servo sequence.
 
 void setup() {
   // Configure output pins.
-  pinMode(S_IN, OUTPUT);
-  pinMode(CLK, OUTPUT);
+  pinMode(DATA, OUTPUT);
+  pinMode(SHIFT, OUTPUT);
   pinMode(STROBE, OUTPUT);
   pinMode(HOME, OUTPUT);
 
   // Configure input pins.
   pinMode(ALL_HOMED, INPUT);
-  pinMode(DIAG_3, INPUT);
-  pinMode(DIAG_2, INPUT);
-  pinMode(DIAG_1, INPUT);
+
+  // Configure interrupt pins.
+  pinMode(CHANGE_DELAY, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(CHANGE_DELAY), change_delay, FALLING);
 }
 
 void loop() {
@@ -57,10 +58,8 @@ int SERVO_ROT[2] = {2,2};
   //SERVO_ROT[1]=8;
   //SERVO_ROT[0]=4096;
 
-
-
 //DIAGNOSTIC
-int i_all_homed, button_3, button_2, button_1;
+int i_all_homed;
 
 //Count
 int Rotations = 0;
@@ -72,9 +71,6 @@ while(Stop_Rotate == 0){
   
   // Read the state of the pushbuttons:
   i_all_homed = digitalRead(ALL_HOMED);
-  button_3 = digitalRead(DIAG_3);
-  button_2 = digitalRead(DIAG_2);
-  button_1 = digitalRead(DIAG_1);
 
   //If homing, send out the home command.
   //if(Home_Char == 0){
@@ -89,13 +85,13 @@ while(Stop_Rotate == 0){
 
     // Loop for each data point and send down the shift register, 4 data points per servo.
     for(int j = c_SERVO_POS_COUNT - 1; j >= 0; j--){
-      digitalWrite(S_IN, bitRead(SERVO_OUT[i], j));
-      pin_Pulse(CLK, c_DELAY, HIGH);
+      digitalWrite(DATA, bitRead(SERVO_OUT[i], j));
+      pin_Pulse(SHIFT, v_DELAY, HIGH);
     }              
   }
 
   // Once data has been updated in the shift registers, strobe to update outputs     
-  pin_Pulse(STROBE, c_DELAY, HIGH);
+  pin_Pulse(STROBE, v_DELAY, HIGH);
   Rotations++;
 
   // Default to stop rotation, override if continued rotation is necessary.
@@ -125,7 +121,7 @@ while(Stop_Rotate == 0){
   }
 }
 
-delay(5000);
+//delay(5000);
 }
 
 byte servo_Rotate(byte* SERVO, byte SEQ, bool DIR){
@@ -164,4 +160,17 @@ void pin_Pulse(int pin, int delay, bool high){
   }
 
   return;
+}
+
+//--------------------Interrupts----------------------//
+void change_delay(){
+
+  if (v_DELAY == c_DELAY_SETTING[0]){
+    v_DELAY = c_DELAY_SETTING[1];
+  }else if(v_DELAY == c_DELAY_SETTING[1]) {
+    v_DELAY = c_DELAY_SETTING[2];
+  }else{
+    v_DELAY = c_DELAY_SETTING[0];  
+  }
+
 }
